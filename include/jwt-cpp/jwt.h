149eee42e3178fb50947b61ba10f9a53ac35d224
Derrity <86703464+Derrity@users.noreply.github.com>
2023-08-12 17:29:25 +0800

    Update jwt.h

--- include/jwt-cpp/jwt.h
+++ include/jwt-cpp/jwt.h
@@ -3263,13 +3263,13 @@ namespace jwt {
 		 * \param jwt Token to check
 		 * \throw token_verification_exception Verification failed
 		 */
-        const bool verify(const decoded_jwt<json_traits>& jwt) const {
+         bool verify(const decoded_jwt<json_traits>& jwt) const {
             try {
                 std::error_code ec;
                 verify(jwt, ec);
                 error::throw_if_error(ec);
                 return true;
-            }catch(const std::exception&){
+            }catch(const std::exception& e){
                 return false;
             }
 		}
